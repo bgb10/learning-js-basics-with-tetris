0f80d548a8d13360459e58ada7ac9249b635066e
Guest <guest-vobn5k@h26118.sgb.skir.kr>
2021-11-20 19:21:28 +0900

    add rotation system

--- board.js
+++ board.js
@@ -9,33 +9,51 @@ class Board {
     // 0으로 채워진 행렬을 얻는다.
     getEmptyBoard() {
         return Array.from(
-            {length: ROWS}, () => Array(COLS).fill(0)
+            { length: ROWS }, () => Array(COLS).fill(0)
         );
     }
 
     valid(p) {
         return p.shape.every((row, dy) => {
-          return row.every((value, dx) => {
-            let x = p.x + dx;
-            let y = p.y + dy;
-            return (
-              this.isEmpty(value) ||
-             (this.insideWalls(x) &&
-              this.aboveFloor(y))
-            );
-          });
+            return row.every((value, dx) => {
+                let x = p.x + dx;
+                let y = p.y + dy;
+                return (
+                    this.isEmpty(value) ||
+                    (this.insideWalls(x) &&
+                        this.aboveFloor(y))
+                );
+            });
         });
-      }
+    }
 
-      isEmpty(value) {
-          return value == 0;
-      }
+    rotate(p) {
+        // deep copy 및 immtability 를 위해 JSON 으로 복사
+        let clone = JSON.parse(JSON.stringify(p));
 
-      insideWalls(x) {
-          return x >= 0 && x < COLS;
-      }
+        // 행렬을 변환한다. p는 Piece의 인스턴스이다.
+        for (let y = 0; y < p.shape.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+                [p.shape[x][y], p.shape[y][x]] =
+                    [p.shape[y][x], p.shape[x][y]];
+            }
+        }
 
-      aboveFloor(y) {
-          return y < ROWS;
-      }
+        // 열 순서대로 뒤집는다.
+        p.shape.forEach(row => row.reverse());
+
+        return clone;
+    }
+
+    isEmpty(value) {
+        return value == 0;
+    }
+
+    insideWalls(x) {
+        return x >= 0 && x < COLS;
+    }
+
+    aboveFloor(y) {
+        return y < ROWS;
+    }
 }
