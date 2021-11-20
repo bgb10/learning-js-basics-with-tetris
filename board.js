c09ef66da0c6d838088df654f748152fbf7404bc
Guest <guest-vobn5k@h26118.sgb.skir.kr>
2021-11-20 19:14:50 +0900

    add collision detection

--- board.js
+++ board.js
@@ -14,6 +14,28 @@ class Board {
     }
 
     valid(p) {
-        return true;
-    }
+        return p.shape.every((row, dy) => {
+          return row.every((value, dx) => {
+            let x = p.x + dx;
+            let y = p.y + dy;
+            return (
+              this.isEmpty(value) ||
+             (this.insideWalls(x) &&
+              this.aboveFloor(y))
+            );
+          });
+        });
+      }
+
+      isEmpty(value) {
+          return value == 0;
+      }
+
+      insideWalls(x) {
+          return x >= 0 && x < COLS;
+      }
+
+      aboveFloor(y) {
+          return y < ROWS;
+      }
 }
